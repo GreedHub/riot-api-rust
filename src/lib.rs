49b6f48cb5e1af94716602720c3446ba89fe250e
Lucas Julian Guerci <lucas.guerci@gmail.com>
2022-12-04 19:34:21 -0300

    added base node test app

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 use sysinfo::{ProcessExt, System, SystemExt};
 use std::os::raw::c_char;
 use std::ffi::CString;
+use std::ffi::CStr;
 
 #[repr(C)]
 pub struct RiotApiInfo  {
-    pub port: *mut c_char,
+    pub port: i32,
     pub token: *mut c_char,
 }
 
@@ -12,11 +13,16 @@ fn to_c_str(s:&str)->*mut c_char{
     CString::new(s).unwrap().into_raw()
 }
 
+fn c_str_to_str(cs:*mut c_char)->String{
+    let cstring = unsafe {CStr::from_ptr(cs)};
+    cstring.to_str().unwrap().to_owned()
+}
+
 
 #[no_mangle]
-pub extern fn  get_riot_token() ->  RiotApiInfo{
+pub extern fn  get_riot_token(_n:i32) ->  RiotApiInfo{
     let mut riot_api_info =RiotApiInfo{
-        port: to_c_str(""),
+        port: 0,
         token: to_c_str(""),
     };
 
@@ -26,8 +32,7 @@ pub extern fn  get_riot_token() ->  RiotApiInfo{
     for p in system.processes_by_name("LeagueClient.exe") {
         for c in p.cmd(){
             if c.contains("--riotclient-app-port"){
-                let p = c.split("=").last().unwrap();
-                riot_api_info.port= to_c_str(p);
+                riot_api_info.port= c.split("=").last().unwrap().parse::<i32>().unwrap();
 
             }
             if c.contains("--riotclient-auth-token") {
@@ -38,4 +43,9 @@ pub extern fn  get_riot_token() ->  RiotApiInfo{
     }
 
     riot_api_info
+}
+
+#[no_mangle]
+pub extern fn sum(n1:i32,n2:i32)->i32{
+    n1+n2
 }
